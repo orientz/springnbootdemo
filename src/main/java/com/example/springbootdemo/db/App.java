6277745dd2f9282d257ac95267a07ede579e8d9b
zeng_yy <zeng_yy@ctrip.com>
2019-05-20 15:05:08 +0800

    mybatis代码生成

---
+++ src/main/java/com/example/springbootdemo/db/App.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo.db;
+
+
+import org.mybatis.generator.api.ShellRunner;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            args = new String[] { "-configfile", "src\\main\\resources\\generator.xml", "-overwrite" };
+            ShellRunner.main(args);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
